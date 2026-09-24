45ebac119e881c83aa10c7a32e2854f3f11fdad0
AlokSingh122 <aloksinghsisodiya90@gmail.com>
2026-09-24 16:29:12 +0530

    Revert "Add modular Terraform architecture"
    This reverts commit 5be953523dcd2cdf2533ba132ac7c7f4cab6d404.

--- ec2/ami.tf
+++
@@ -1,7 +0,0 @@
-data "aws_ssm_parameter" "amazon_linux_2023" {
-  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
-}
-
-locals {
-  ami_id = coalesce(var.ami_id, data.aws_ssm_parameter.amazon_linux_2023.value)
-}
